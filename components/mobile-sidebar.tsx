1b71eac452c69e902ab140ef8ddb7fc50e61d8b2
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-08-19 08:50:06 +0200

    added the logic of the mobile-sidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Menu } from "lucide-react";
 
-const mobile-sidebar = () => {
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sidebar } from "@/components/sidebar";
+
+export const MobileSidebar = ({
+  isPro
+}: {
+  isPro: boolean;
+}) => {
   return (
-    <div>mobile-sidebar</div>
-  )
-}
-
-export default mobile-sidebar
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
+        <Sidebar isPro={isPro} />
+      </SheetContent>
+    </Sheet>
+  );
+};
