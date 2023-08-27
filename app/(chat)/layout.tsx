6da37e7813be22085265e868c77c6dec92b97878
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-08-27 08:21:32 +0200

    implemented the logic behind the layout of the chat route

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
+const ChatLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="mx-auto max-w-4xl h-full w-full">{children}</div>;
+};
 
-const layout = () => {
-  return (
-    <div>layout</div>
-  )
-}
-
-export default layout
+export default ChatLayout;
