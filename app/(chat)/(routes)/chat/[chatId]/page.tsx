285060448f2e12e9d6fb571bf1540ceafbc4ba25
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-08-29 08:50:19 +0200

    implemented the logic for the chat page route

--- app/(chat)/(routes)/chat/[chatId]/page.tsx
+++ app/(chat)/(routes)/chat/[chatId]/page.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { redirect } from "next/navigation";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+import prismadb from "@/lib/prismadb";
+
+import { ChatClient } from "./components/client";
+
+interface ChatIdPageProps {
+  params: {
+    chatId: string;
+  };
 }
 
-export default page
+const ChatIdPage = async ({ params }: ChatIdPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirectToSignIn();
+  }
+
+  const companion = await prismadb.companion.findUnique({
+    where: {
+      id: params.chatId,
+    },
+    include: {
+      messages: {
+        orderBy: {
+          createdAt: "asc",
+        },
+        where: {
+          userId,
+        },
+      },
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  });
+
+  if (!companion) {
+    return redirect("/");
+  }
+
+  return <ChatClient companion={companion} />;
+};
+
+export default ChatIdPage;
